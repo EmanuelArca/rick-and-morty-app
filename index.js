fbd2e8c8978e48bac318d611c884f6e61f1eeecf
Daniel Greßmann <daniel@gressmann.online>
2025-02-26 13:16:52 +0100

    update task 4 maxPages and dynamic URL

--- index.js
+++ index.js
@@ -14,17 +14,29 @@ const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
+let characterUrl = "https://rickandmortyapi.com/api/character";
 
 // States
-const maxPage = 1;
-const page = 1;
+let maxPage = 1;
+let page = 1;
 const searchQuery = "";
 
 // Functions
+
+function paging() {
+  characterUrl = `${characterUrl}?page=${page}`;
+  return characterUrl;
+}
+
 async function fetchCharacters() {
-  const response = await fetch("https://rickandmortyapi.com/api/character");
+  const response = await fetch(paging());
   const data = await response.json();
 
+  // Aktualisierung der "maxPages"
+  maxPage = data.info.pages;
+  pagination.textContent = `${page} / ${maxPage}`;
+
+  // Charaktere erhalten
   data.results.map((characterElement) => {
     // Deklaration der Objektelemente die durch die API gelesen werden
     const {
@@ -32,9 +44,9 @@ async function fetchCharacters() {
       name: imageAltText,
       name: nameCardTitle,
       status: statusCardInfo,
-      type: typeCardInfo, 
+      type: typeCardInfo,
     } = characterElement;
-    
+
     // Destruct "episode" separat, weil "length" operation nicht im destruct funktionierte
     const { episode } = characterElement;
     const occurencesCardInfo = episode.length;
